449636b7291c3f6dab7ad9265ae6f18248cf864f
FacundoLG <facu-judoca@hotmail.com.ar>
2021-09-02 22:36:13 -0300

    run

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,43 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{
+public class PlayerMovement : MonoBehaviour{
     private Vector3 PlayerMovementInput;
     private Vector2 PlayerMouseInput;
     private float XRotation;
-
     [SerializeField] private Transform PlayerCamera;
     [SerializeField] private Rigidbody PlayerBody;
     [Space]
+    [SerializeField] private float DistanceToGround;
+    [Space]
     [SerializeField] private float Speed;
+    [SerializeField] private float RunSpeed;
     [SerializeField] private float Sensitivty;
     [SerializeField] private float Jumpforce;
 
-    void Start()
-    {
+    void Start(){
         Cursor.visible = false;
+       
     }
 
-    private void Update()
-    {
+    private void Update(){
         PlayerMovementInput = new Vector3(Input.GetAxis("Horizontal"), 0f ,Input.GetAxis("Vertical"));
         PlayerMouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
         MovePlayer();
         MovePlayerCamera();
     }
-    private void MovePlayer()
-    {
-        Vector3 MoveVector = transform.TransformDirection(PlayerMovementInput) * Speed;
+    private void MovePlayer(){
+        float Velocity;
+        if (Input.GetKey(KeyCode.LeftShift)) {
+            Velocity = RunSpeed;
+        }else {
+            Velocity = Speed;
+        }
+        Vector3 MoveVector = transform.TransformDirection(PlayerMovementInput) * Velocity;
         PlayerBody.velocity = new Vector3(MoveVector.x, PlayerBody.velocity.y,MoveVector.z);
 
+        
+       
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            PlayerBody.AddForce(Vector3.up * Jumpforce, ForceMode.Impulse);
+                PlayerBody.AddForce(Vector3.up * Jumpforce, ForceMode.Impulse);
         }
     }
-    private void MovePlayerCamera()
-    {
+    private void MovePlayerCamera(){
         XRotation -= PlayerMouseInput.y * Sensitivty;
 
         transform.Rotate(0f, PlayerMouseInput.x * Sensitivty, 0f);
